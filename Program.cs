4074aefd9fc375d70e89893a42a739e929344b28
Emil Pedersen <steamman0103@gmail.com>
2026-02-11 23:12:31 +0100

    Update with get and set for the user in database

--- Program.cs
+++ Program.cs
@@ -1,5 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using backend.getdata;
+using Models.User;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Users user = new Users
+        {
+            Id = 1,
+            Email = "test@test.com",
+            Password = "password",
+            FirstName = "test",
+            Username = "tester",
+            Age = 23
+
+        };
+
+
+        DataUser dataUser = new DataUser();
+
+
+        bool v = dataUser.setUsers(user);
+
+
+
+    }
+}
+
 
 
 // TODO run the database please use 
---
+++ getdata/User.cs
@@ -0,0 +1,97 @@
+﻿using Models.User;
+
+namespace backend.getdata
+{
+
+    public class DataUser
+    {
+        public bool setUsers(Users user)
+        {
+            try
+            {
+                DatabaseContext db = new DatabaseContext();
+                db.User.Add(user);
+
+                db.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+
+            }
+        }
+
+
+        public Users? getUserByEmail(string email)
+        {
+            try
+            {
+                DatabaseContext db = new DatabaseContext();
+                Users? user = db.User.FirstOrDefault(u => u.Email == email);
+                
+                
+                if(user != null)
+                {
+                    return user;
+                } else
+                {
+                    throw new Exception("No User with Email");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public Users? getUserByUserName(string email)
+        {
+            try
+            {
+                DatabaseContext db = new DatabaseContext();
+                Users? user = db.User.FirstOrDefault(u => u.Username == email);
+
+                if (user != null)
+                {
+                    return user;
+                }
+                else
+                {
+                    throw new Exception("No User with Username");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+
+
+
+        public List<Users>? getAllUsers()
+        {
+            try
+            {
+                DatabaseContext db = new DatabaseContext();
+                List<Users> users = db.User.ToList();
+                return users;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+
+
+
+
+    }
+}
