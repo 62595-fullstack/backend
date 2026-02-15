1cf378b52d89f7d361f85f6c74b8dc428905440b
Toby <60399231+TobyNatooor@users.noreply.github.com>
2026-02-15 22:29:06 +0100

    .

--- Program.cs
+++ Program.cs
@@ -19,17 +19,18 @@ var connString = $@"Host={config["host"]};
 
 await using var conn = new NpgsqlConnection(connString);
 await conn.OpenAsync();
+// using var context = new DatabaseContext();
 
-// await using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
-// {
-//     cmd.Parameters.AddWithValue("p", "Hello world");
-//     await cmd.ExecuteNonQueryAsync();
-// }
-//
-// // Retrieve all rows
-// await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
-// await using (var reader = await cmd.ExecuteReaderAsync())
-// {
-// while (await reader.ReadAsync())
-//     Console.WriteLine(reader.GetString(0));
-// }
+await using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
+{
+    cmd.Parameters.AddWithValue("p", "Hello world");
+    await cmd.ExecuteNonQueryAsync();
+}
+
+// Retrieve all rows
+await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+while (await reader.ReadAsync())
+    Console.WriteLine(reader.GetString(0));
+}
