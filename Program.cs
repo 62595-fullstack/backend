fd0e2a5f7a4a61b43fe39ddaea29b118bc69f9f3
Toby <60399231+TobyNatooor@users.noreply.github.com>
2026-02-15 21:15:30 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-// TODO run the database please use 
+﻿// TODO run the database please use 
 // docker run --name BookFace -e POSTGRES_PASSWORD=facebook -d -p 5432:5432 postgres
-// dotnet ef datrabase update
+// dotnet ef database update
+
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+var connString = "Host=localhost;Username=facebook;Password=1234;Database=testdb";
+
+await using var conn = new DatabaseContext(connString);
+await conn.OpenAsync();
+
+// // Insert some data
+// await using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
+// {
+//     cmd.Parameters.AddWithValue("p", "Hello world");
+//     await cmd.ExecuteNonQueryAsync();
+// }
+//
+// // Retrieve all rows
+// await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+// await using (var reader = await cmd.ExecuteReaderAsync())
+// {
+// while (await reader.ReadAsync())
+//     Console.WriteLine(reader.GetString(0));
+// }
