0f71ae46b9465eaf5be2e27460e5bc41343f4566
SMan0103 <steamman0103@gmail.com>
2026-02-18 10:34:19 +0100

    update

--- getdata/Organization.cs
+++ getdata/Organization.cs
@@ -14,12 +14,33 @@ namespace backend.getdata
 
                 db.SaveChanges();
 
+
                 return organizations;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 return organizations;
+            }
+        }
+
+
+        public Organizations GetOrganizationByName(string OrganizationName)
+        {
+            try
+            {
+                DatabaseContext db = new DatabaseContext();
+                
+                db.Add(OrganizationName);
+
+                db.SaveChanges();
+                
+                return db.Organization.TakeLast(1).First();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
             }
         }
     }
