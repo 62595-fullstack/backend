329ffdb1a8dc5a091e2a557c957f970918a2accc
SMan0103 <steamman0103@gmail.com>
2026-02-23 12:01:00 +0100

    Update to organizationEvents and UserOrganizationBinding

---
+++ getdata/OrganizationEvents.cs
@@ -0,0 +1,29 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using Models.OrganizationEvent;
+using Models.User;
+
+namespace backend.getdata
+{
+    public class DataOrganizationEvents
+    {
+        public async Task<List<OrganizationEvents>> getOrganizationEvents(int organizationId)
+        {
+            try
+            {
+                DatabaseContext db = new DatabaseContext();
+                
+
+
+                return await db.OrganizationEvent.Where(x => x.OrganizationId == organizationId).ToListAsync();;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<OrganizationEvents>();
+            }
+        }
+
+    }
+}
