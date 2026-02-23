81e26b4d7031024c3102541b1e1a4d85ce127926
Toby <60399231+TobyNatooor@users.noreply.github.com>
2026-02-23 11:21:18 +0100

    removed test

--- Program.cs
+++ Program.cs
@@ -11,18 +11,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-string json = @"{
-        'Id': '12345',
-        'Title': 'Title',
-        'BodyText': 'Lorem ipsum',
-}";
-
-PostsDto p = JsonConvert.DeserializeObject<PostsDto>(json);
-
-Console.WriteLine(p.Id);
-Console.WriteLine(p.Title);
-Console.WriteLine(p.BodyText);
-
 app.MapGet("/posts", string () =>
 {
 	try
