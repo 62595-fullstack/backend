2b62d5d06c3f0a7f78454e5a1c067b4025241dbb
Gustav Aggeboe <gustavaggeboe@hotmail.com>
2026-03-16 13:37:43 +0100

    Make dummy data creation non-destructive

--- DummyData.cs
+++ DummyData.cs
@@ -11,7 +11,19 @@ class DummyData
 {
 	private static async Task Add<T>(DatabaseContext db, params T[] entities) where T : class
 	{
-		await db.Set<T>().AddRangeAsync(entities);
+		var entityType = db.Model.FindEntityType(typeof(T))!;
+		var primaryKey = entityType.FindPrimaryKey()!;
+
+		foreach (var entity in entities)
+		{
+			var keyValues = primaryKey.Properties
+				.Select(p => p.PropertyInfo!.GetValue(entity)!)
+				.ToArray();
+
+			if (await db.Set<T>().FindAsync(keyValues) == null)
+				await db.Set<T>().AddAsync(entity);
+		}
+
 		await db.SaveChangesAsync();
 	}
 
@@ -19,46 +31,28 @@ class DummyData
 	{
 		using (var db = new DatabaseContext())
 		{
-			db.Post.RemoveRange(db.Post);
-			db.User.RemoveRange(db.User);
-			db.Role.RemoveRange(db.Role);
-			db.Organization.RemoveRange(db.Organization);
-			db.OrganizationPost.RemoveRange(db.OrganizationPost);
-			db.OrganizationEvent.RemoveRange(db.OrganizationEvent);
-			db.UserEventBinding.RemoveRange(db.UserEventBinding);
-			db.UserOrganizationBinding.RemoveRange(db.UserOrganizationBinding);
-
 			await Add(db,
-				new Users { Email = "crazyfrog@hotmail.com", Password = "bingbing", FirstName = "Crazy", Username = "Frog", Age = 2 },
+				new Users { Id = 999, Email = "crazyfrog@hotmail.com", Password = "bingbing", FirstName = "Crazy", Username = "Frog", Age = 2 },
 				new Users { Id = 1000, Email = "bbbenson@hotmail.com", Password = "1234", FirstName = "Berry B.", Username = "Benson", Age = 2 }
 			);
 
 			await Add(db,
-				new Organizations { Id = 1000, Name = "NetCompany", CreatedDate = DateTime.UtcNow },
 				new Organizations { Id = 123, Name = "Friske Gutter", CreatedDate = DateTime.UtcNow },
-				new Organizations { Name = "HollyWood", CreatedDate = DateTime.UtcNow }
+				new Organizations { Id = 998, Name = "HollyWood", CreatedDate = DateTime.UtcNow },
+				new Organizations { Id = 1000, Name = "NetCompany", CreatedDate = DateTime.UtcNow }
 			);
 
 			await Add(db,
-				new Roles { Id = 1000, Name = "Admin" },
-				new Roles { Name = "Employee" }
+				new Roles { Id = 999, Name = "Employee" },
+				new Roles { Id = 1000, Name = "Admin" }
 			);
 
 			await Add(db,
-				new UserOrganizationBindings { Id = 1000, UserId = 1000, OrganizationId = 1000, RoleId = 999 },
-				new UserOrganizationBindings { Id = 123, UserId = 123, OrganizationId = 123, RoleId = 999 }
+				new UserOrganizationBindings { Id = 123, UserId = 123, OrganizationId = 123, RoleId = 999 },
+				new UserOrganizationBindings { Id = 1000, UserId = 1000, OrganizationId = 1000, RoleId = 999 }
 			);
 
 			await Add(db,
-				new OrganizationEvents {
-					Id = 1000,
-					OrganizationId = 1000,
-					Title = "Åbent hus i NetCompany",
-					CreatedDate = DateTime.UtcNow,
-					StateDate = DateTime.UtcNow,
-					AgeLimit = 18,
-					UserOrganizationBindingId = 1000
-				},
 				new OrganizationEvents
 				{
 					Id = 123,
@@ -70,18 +64,28 @@ class DummyData
 					StateDate = DateTime.UtcNow,
 					AgeLimit = 18,
 					UserOrganizationBindingId = 123
+				},
+				new OrganizationEvents
+				{
+					Id = 1000,
+					OrganizationId = 1000,
+					Title = "Åbent hus i NetCompany",
+					CreatedDate = DateTime.UtcNow,
+					StateDate = DateTime.UtcNow,
+					AgeLimit = 18,
+					UserOrganizationBindingId = 1000
 				}
 			);
 
 			await Add(db,
-				new UserEventBindings { Id = 1000, UserId = 1000, OrganizationEventsId = 1000 },
-				new UserEventBindings { Id = 123, UserId = 123, OrganizationEventsId = 123 }
+				new UserEventBindings { Id = 123, UserId = 123, OrganizationEventsId = 123 },
+				new UserEventBindings { Id = 1000, UserId = 1000, OrganizationEventsId = 1000 }
 			);
 
 			await Add(db,
-				new Posts { Title = "Fist Event Post", CreatedDate = DateTime.UtcNow, UserId = 1000, OrganizationEventId = 1000 },
-				new Posts { Id = 1000, Title = "Hello World", CreatedDate = DateTime.UtcNow, UserId = 1000, OrganizationEventId = 1000 },
-				new Posts { Title = "Bee Movie Trailer Night", CreatedDate = DateTime.UtcNow, UserId = 1000, OrganizationEventId = 1000 }
+				new Posts { Id = 998, Title = "Fist Event Post", CreatedDate = DateTime.UtcNow, UserId = 1000, OrganizationEventId = 1000 },
+				new Posts { Id = 999, Title = "Bee Movie Trailer Night", CreatedDate = DateTime.UtcNow, UserId = 1000, OrganizationEventId = 1000 },
+				new Posts { Id = 1000, Title = "Hello World", CreatedDate = DateTime.UtcNow, UserId = 1000, OrganizationEventId = 1000 }
 			);
 
 			await Add(db,
