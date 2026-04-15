42912cf8a3873f1af910125a303d579beaae4dd9
Toby <60399231+TobyNatooor@users.noreply.github.com>
2026-04-15 10:20:50 +0200

    change endpoint and added test

--- src/Dto.cs
+++ src/Dto.cs
@@ -3,3 +3,4 @@ namespace Dto;
 public record LoginCredentialsDto(string Email, string Password);
 public record RegisterCredentialsDto(string Email, string Password, string FirstName, int Age);
 public record PostDto(string Title, string BodyText, int UserId, int OrganizationEventId);
+public record AttachmentDto(string FileName, string FileType, byte[] Content);
