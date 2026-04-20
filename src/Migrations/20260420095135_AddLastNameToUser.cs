b6b1849e023b7bb3c0359da5dacd60dbe5aea24a
Gustav Aggeboe <gustavaggeboe@hotmail.com>
2026-04-20 11:55:47 +0200

    Add migration to add `LastName` column to `Users` table and apply `MaxLength(128)` constraint to `FirstName` and `LastName` columns

---
+++ src/Migrations/20260420095135_AddLastNameToUser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastNameToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "User",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "User",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "User",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
+        }
+    }
+}
